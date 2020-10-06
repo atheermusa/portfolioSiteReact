1a822cf798678d8153424bea8ff58d5409bfcf98
Atheer <atheerm96@googlemail.com>
2020-10-06 23:38:21 +0100

    styling changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render(){
 
     return (
-      <div className= {this.state.setDarkMode ? "dark-mode" : "light-mode"}>
+      <div className= {this.state.setDarkMode ? "dark-mode" : "light-mode"} id="site-container">
         <header>
           <NavBar clickHandler={this.darkModeToggle}/>
           {/* <Title /> */}
@@ -39,7 +39,9 @@ class App extends Component {
 
           
         </main>
+        <div id= "footer-container">
         <Footer />
+        </div>
       </div>
     );
   }
