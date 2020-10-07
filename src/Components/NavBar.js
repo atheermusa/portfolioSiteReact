184bccaaabc9a1aec21139b4b98e1a9b1a8de91e
Atheer <atheerm96@googlemail.com>
2020-10-07 22:08:53 +0100

    remove comments

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,6 +16,7 @@ export default class NavBar extends Component {
     mouseLeaveHandler = (e) => {
         setTimeout(() => this.setState({logoClass: "shake"}), 0);
     };
+
     render() {
         return (
             <div className= "container">
@@ -28,7 +29,7 @@ export default class NavBar extends Component {
                     </Link>
                 </div>
                 <nav className="nav">
-                    <NavLink className="navlink" activeClassName="current" to="/" href="about">Home</NavLink>
+                    <NavLink className="navlink" activeClassName="current" to="/" href="about" >Home</NavLink>
                     <NavLink className="navlink" activeClassName="current" to="/about" href="about">About</NavLink>
                     <NavLink className="navlink" activeClassName="current" to="/projects" href="projects">Projects</NavLink>
                     <NavLink className="navlink" activeClassName="current" to="/articles">Articles</NavLink>
@@ -37,49 +38,3 @@ export default class NavBar extends Component {
         );
     }
 }
-
-
-
-// const bulbLogo = document.getElementById("logo")
-
-// const state = {
-//     logoClass: "shake"
-// }
-
-// const hoverHandler = (e) => {
-//     state.logoClass.setS
-  
-//   // -> triggering reflow /* The actual magic */
-//   // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-//   // Oops! This won't work in strict mode. Thanks Felis Phasma!
-//   // element.offsetWidth = element.offsetWidth;
-//   // Do this instead:
-//   void bulbLogo.offsetWidth;
-  
-//   // -> and re-adding the class
-//   bulbLogo.classList.add("shake");
-// };
-
-// const NavBar = ({ clickHandler }) => {
-    
-//     return (
-//         <div className= "container">
-//             <div className="logoContainer">
-//                 <div onClick={clickHandler} id="logo" className={state.logoClass} onMouseEnter={hoverHandler}>
-//                     <Logo />
-//                 </div>
-//                 <Link to="/" className="logoName">
-//                 <p>Atheer Musa</p>
-//                 </Link>
-//             </div>
-//             <nav className="nav">
-//                 <NavLink className="navlink" activeClassName="current" to="/" href="about">Home</NavLink>
-//                 <NavLink className="navlink" activeClassName="current" to="/about" href="about">About</NavLink>
-//                 <NavLink className="navlink" activeClassName="current" to="/projects" href="projects">Projects</NavLink>
-//                 <NavLink className="navlink" activeClassName="current" to="/articles">Articles</NavLink>
-//             </nav>
-//         </div>
-//     );
-// }
-
-// export default NavBar;
