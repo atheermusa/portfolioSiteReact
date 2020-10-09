becb9469f25fc97c3804a10d0bed26ef56b225de
Atheer <atheerm96@googlemail.com>
2020-10-09 13:16:27 +0100

    add fix for switching images when changing projects

--- src/Components/ProjectImageContainer.js
+++ src/Components/ProjectImageContainer.js
@@ -6,15 +6,16 @@ export default class ProjectImageContainer extends Component {
 
 
     state = {
+        imgsrc : this.props.imgsrc,
         currentImgSrc : `${this.props.imgsrc[0]}`,
         imgIdx: 0,
         currentImgDesc : `${this.props.imgdescription[0]}`,
+        isNewProject : true
     }
-
-    // need to make the function async so that it auto updates
-    // need to add hide buttons when reach min/max using an else 
     imagesSwitchPlus = async () => {
-
+            await this.setState({
+                isNewProject : false
+            })
         if (this.state.imgIdx < this.props.imgsrc.length - 1)
             await this.setState(prevState => ({
                 imgIdx: ++prevState.imgIdx
@@ -36,13 +37,23 @@ export default class ProjectImageContainer extends Component {
                 currentImgDesc : this.props.imgdescription[this.state.imgIdx]   
             })}
     }
+
+static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.imgsrc !== nextProps.imgsrc) {
+      return {
+        imgsrc : nextProps.imgsrc,
+        isNewProject : true,
+        imgIdx: 0
+        }
+      };
+    }
     render() {
         return (
             <div id="project-img-container">
-                <img src={this.state.currentImgSrc} id="project-img" alt="this is a screenshot of the selected project"></img>
+                <img src={ this.state.isNewProject ? this.props.imgsrc[0] : this.state.currentImgSrc} id="project-img" alt="this is a screenshot of the selected project"></img>
                 <div id="project-img-text-container">
                     <p id="project-img-counter">{this.state.imgIdx + 1}/{this.props.imgsrc.length}</p>
-                    <p id="project-img-decription">{this.state.currentImgDesc}</p>
+                    <p id="project-img-decription">{ this.state.isNewProject ? this.props.imgdescription[0] : this.state.currentImgDesc}</p>
                 </div>
                 <div id="previous-img"className="image-switcher-button" onClick={this.imagesSwitchMinus}>{'<'}</div>
                 <div id="next-img" className="image-switcher-button" onClick={this.imagesSwitchPlus}>{'>'}</div>
--- src/Containers/ProjectsContainer.js
+++ src/Containers/ProjectsContainer.js
@@ -26,6 +26,7 @@ export default class ProjectsContainer extends Component {
                 Link = "https://github.com/atheermusa/captains-log"
                 imgsrc = {["https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230807/Screenshot_2020-10-09_at_08.53.03-min_bqe76t.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230802/Screenshot_2020-10-09_at_08.53.19-min_hclsn3.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230501/Screenshot_2020-10-09_at_08.53.53_f0etha.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230502/Screenshot_2020-10-09_at_08.54.17_weuzmj.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230503/Screenshot_2020-10-09_at_08.55.05_dilp5q.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230504/Screenshot_2020-10-09_at_08.56.16_wkhccm.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230502/Screenshot_2020-10-09_at_08.56.31_iss9zf.png"]}
                 imgdescription = {["screenshot showing login/registration page", "screenshot showing dashboard that greets user upn logging in", "screenshot showing form to add new players to the squad", "screenshot displaying a populated player list", "screenshot demonstrating drag and drop team builder", "screenshot showing match report functionality along with pdf download and mail functionality", "screenshot of match report pdf"]}
+                IsNewProject = {true}
                 />
                 ;
                 case 2:
@@ -34,7 +35,8 @@ export default class ProjectsContainer extends Component {
                     Description = "Apple a Day is a habit tracking app. Once logged in the user can add new habits to track and set a frequency at which they want to complete that habit. Users will then sign in and click the “Complete Habit” button and will add to their streak in doing so. Failure to complete a habit on time will cause the streak to reset."
                     Link = "https://github.com/atheermusa/LAP-3-App"
                     imgsrc = {["https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230490/Screenshot_2020-10-09_at_08.42.24_eaf40z.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230490/Screenshot_2020-10-09_at_08.42.53_nthm1c.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230493/Screenshot_2020-10-09_at_08.43.53_utn6py.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230491/Screenshot_2020-10-09_at_08.43.04_y0emsk.png"]}
-                      imgdescription = {["screenshot showing login/registration page", "screenshot of dashboard", "New habit form", "user profile page"]}
+                    imgdescription = {["screenshot showing login/registration page", "screenshot of dashboard", "New habit form", "user profile page"]}
+                    IsNewProject = {true}
                     />
                     ;
                     case 3:
@@ -44,6 +46,7 @@ export default class ProjectsContainer extends Component {
                       Link = "https://github.com/atheermusa/fp_lap_1_portfolio_week"
                       imgsrc = {["https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602191101/Screenshot_2020-10-08_at_22.04.48_n6gh9j.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602191197/Screenshot_2020-10-08_at_22.06.24_srjvj4.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602197593/Screenshot_2020-10-08_at_21.21.12_kidl0e.png"]}
                       imgdescription = {["screenshot showing logo styling, new post form and gif functionality", "screenshot showing a test post with a gif attatched, how it looks when a user reacts with an emoji and what the comment section looks like", "the test post made prior to the demonstration at the end of the project week"]}
+                      IsNewProject = {true}
                       />
                       ;
                       case 4:
@@ -53,6 +56,7 @@ export default class ProjectsContainer extends Component {
                         Link = "https://github.com/atheermusa/quizolation"
                         imgsrc = {["https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602197791/image_nurjrz.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602232031/Screenshot_2020-10-09_at_09.26.11_ybqtg0.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602232031/Screenshot_2020-10-09_at_09.26.24_xxu7lf.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602232031/Screenshot_2020-10-09_at_09.26.39_jki1l9.png"]}
                         imgdescription = {["Quizolation landing page", "screenshot showing game setup form", "screenshot of question and answer selection page", "Game results page"]}
+                        IsNewProject = {true}
                         />
                         ;                   
             default:
@@ -63,6 +67,7 @@ export default class ProjectsContainer extends Component {
                Link = "https://github.com/atheermusa/captains-log"
                imgsrc = {["https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230807/Screenshot_2020-10-09_at_08.53.03-min_bqe76t.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230802/Screenshot_2020-10-09_at_08.53.19-min_hclsn3.png","https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230501/Screenshot_2020-10-09_at_08.53.53_f0etha.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230502/Screenshot_2020-10-09_at_08.54.17_weuzmj.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230503/Screenshot_2020-10-09_at_08.55.05_dilp5q.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230504/Screenshot_2020-10-09_at_08.56.16_wkhccm.png", "https://res.cloudinary.com/dl4pcxjxz/image/upload/v1602230502/Screenshot_2020-10-09_at_08.56.31_iss9zf.png"]}
                imgdescription = {["screenshot showing login/registration page", "screenshot showing dashboard that greets user upn logging in", "screenshot showing form to add new players to the squad", "screenshot displaying a populated player list", "screenshot demonstrating drag and drop team builder", "screenshot showing match report functionality along with pdf download and mail functionality", "screenshot of match report pdf"]}
+               IsNewProject = {true}
                />
                 ;
         }   
